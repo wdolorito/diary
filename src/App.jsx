d85be07281a1b049846971c8c84f73742ca09799
William Dolorito <wdolorito@gmail.com>
2020-11-25 07:54:18 -0800

    App.jsx:   Add Switch to routing for 404 page
    Error.jsx:
      Skeleton 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 
 import Main from './components/views/Main'
 import About from './components/views/About'
+import Error from './components/views/Error'
 import Latest from './components/views/Latest'
 import Posts from './components/views/Posts'
 import PostEditor from './components/Fragments/PostEditor'
@@ -15,30 +16,35 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
-            <Header />
-            <Route
-              exact path='/'
-              render={ (props) => <Main /> }
-            />
-            <Route
-              exact path='/about'
-              render={ (props) => <About /> }
-            />
-            <Route
-              exact path='/latest'
-              render={ (props) => <Latest /> }
-            />
-            <Route
-              exact path='/posts'
-              render={ (props) => <Posts /> }
-            />
-            <Route
-              exact path='/editor'
-              render={ (props) => <PostEditor /> }
-            />
-            <Footer />
-        </div>
+          <div className='App'>
+              <Header />
+              <Switch>
+                <Route
+                  exact path='/'
+                  component={ Main }
+                />
+                <Route
+                  path='/about'
+                  component={ About }
+                />
+                <Route
+                  path='/latest'
+                  component={ Latest }
+                />
+                <Route
+                  path='/posts'
+                  component={ Posts }
+                />
+                <Route
+                  path='/editor'
+                  component={ PostEditor }
+                />
+                <Route
+                  component={ Error }
+                />
+              </Switch>
+              <Footer />
+          </div>
       </Router>
     )
   }
---
+++ src/components/views/Error.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+class Error extends Component {
+  render() {
+    return (
+      <div>This is the error page.</div>
+    )
+  }
+
+}
+
+export default Error
