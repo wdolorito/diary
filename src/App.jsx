db3a8a195233fad54f17058558688fdf844a47d4
William Dolorito <wdolorito@gmail.com>
2021-05-31 08:24:00 -0700

    npm updates
    App.jsx:
      add login target to Router
    
    src/components/views/Login.jsx:
      skeletal output message for login target

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Main from './components/views/Main'
 import About from './components/views/About'
 import Error from './components/views/Error'
 import Latest from './components/views/Latest'
+import Login from './components/views/Login'
 import Posts from './components/views/Posts'
 import PostEditor from './components/Fragments/PostEditor'
 
@@ -15,35 +16,39 @@ class App extends Component {
   render() {
     return (
       <Router>
-          <div className='App'>
-              <Header />
-              <Switch>
-                <Route
-                  exact path='/'
-                  component={ Main }
-                />
-                <Route
-                  path='/about'
-                  component={ About }
-                />
-                <Route
-                  path='/latest'
-                  component={ Latest }
-                />
-                <Route
-                  path='/posts'
-                  component={ Posts }
-                />
-                <Route
-                  path='/editor'
-                  component={ PostEditor }
-                />
-                <Route
-                  component={ Error }
-                />
-              </Switch>
-              <Footer />
-          </div>
+        <div className='App'>
+          <Header />
+          <Switch>
+            <Route
+              exact path='/'
+              component={ Main }
+            />
+            <Route
+              path='/about'
+              component={ About }
+            />
+            <Route
+              path='/latest'
+              component={ Latest }
+            />
+            <Route
+              path='/posts'
+              component={ Posts }
+            />
+            <Route
+              path='/editor'
+              component={ PostEditor }
+            />
+            <Route
+              path='/quixotic'
+              component={ Login }
+            />
+            <Route
+              component={ Error }
+            />
+          </Switch>
+          <Footer />
+        </div>
       </Router>
     )
   }
---
+++ src/components/views/Login.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+class Login extends Component {
+  render() {
+    return (
+      <div>This is the login page.</div>
+    )
+  }
+}
+
+export default Login
