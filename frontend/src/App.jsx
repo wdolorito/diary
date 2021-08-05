1b739761773448a212cba648ed3e29c884b61cf3
William Dolorito <wdolorito@gmail.com>
2021-08-05 14:52:16 -0700

    package-lock.json: package.json:   add 'axios' for network connections
    App.jsx:
      import 'axios'
      prelim skeleton for Login
      prelim Login route declaration
    
    Login.jsx:
      prelim form for Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import axios, { CancelToken } from 'axios'
+
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 
@@ -13,6 +15,117 @@ import Posts from './components/views/Posts'
 import PostEditor from './components/Fragments/PostEditor'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.cancel = null
+
+    this.state = {
+      baseLink: 'http://192.168.15.20:5000/',
+      loginLink: 'login',
+      jwt: '',
+      refreshToken: '',
+      didRefresh: false,
+      logged: false
+    }
+
+    this.baseState = this.state
+  }
+
+  componentDidMount() {
+    this.setState({ loginLink: this.state.baseLink + this.state.loginLink })
+  }
+
+  componentWillUnmount() {
+    if(this.cancel !== null) this.cancel()
+    this.setState(this.baseState)
+  }
+
+  componentDidUpdate() {
+    if(this.state.jwt === '' && !this.state.didRefresh) {
+      this.setState({ didRefresh: true }, this.doRefresh())
+    }
+  }
+
+  setJwt = (newjwt) => {
+    this.setState({ jwt: newjwt })
+  }
+
+  resetJwt = () => {
+    this.setState({ jwt: '',
+                    logged: false })
+  }
+
+  storeToken = (token) => {
+    localStorage.setItem('token', token)
+  }
+
+  getToken = () => {
+    return localStorage.getItem('token')
+  }
+
+  resetToken = () => {
+    localStorage.removeItem('token')
+    this.setState({ didRefresh: false })
+  }
+
+  setLogged = () => {
+    this.setState({ logged: true })
+  }
+
+  doLogin = (log, pass) => {
+    axios({
+      method: 'post',
+      url: this.state.loginLink,
+      cancelToken: new CancelToken(c => this.cancel = c),
+      data: {
+        email: log,
+        password: pass
+      }
+    })
+    .then(
+      (res) => {
+        if(res.status === 200) {
+          this.setJwt(res.data.token)
+          this.setLogged()
+          this.storeToken(res.data.refresh)
+          this.setState({ didRefresh: true })
+        }
+      },
+      (err) => {
+        alert('Unable to log in.')
+        this.resetJwt()
+      }
+    )
+  }
+
+  doRefresh = () => {
+    // const storedToken = this.getToken()
+    // if(storedToken) {
+    //   axios({
+    //     method: 'post',
+    //     url: this.state.refreshLink,
+    //     cancelToken: new CancelToken(c => this.cancel = c),
+    //     headers: {
+    //       'Authorization': 'Bearer ' + storedToken
+    //     }
+    //   })
+    //   .then(
+    //     (res) => {
+    //       if(res.status === 200) {
+    //         this.setJwt(res.data.token)
+    //         this.setLogged()
+    //         this.storeToken(res.data.refresh)
+    //       }
+    //     },
+    //     (err) => {
+    //       this.resetJwt()
+    //       this.resetToken()
+    //     }
+    //   )
+    // }
+  }
+
   render() {
     return (
       <Router>
@@ -39,9 +152,14 @@ class App extends Component {
               path='/editor'
               component={ PostEditor }
             />
-            <Route
-              path='/quixotic'
-              component={ Login }
+            <Route exact path='/quixotic'
+              render={ (props) =>
+                <Login
+                  { ...props }
+                  key='loginDisplay'
+                  logged={ this.state.logged }
+                  doLogin={ this.doLogin }
+                /> }
             />
             <Route
               component={ Error }
--- frontend/src/components/views/Login.jsx
+++ frontend/src/components/views/Login.jsx
@@ -1,10 +1,92 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.baseState = this.state
+  }
+
+  componentWillUnmount() {
+    this.resetForm()
+  }
+
+  resetForm = () => {
+    this.setState(this.baseState)
+  }
+
+  handleInput = (e) => {
+    this.setState({ [ e.target.name ]: e.target.value })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+
+    this.props.doLogin(this.state.email, this.state.password)
+    this.resetForm()
+    this.props.history.push('/')
+  }
+
   render() {
-    return (
-      <div>This is the login page.</div>
-    )
+    if(this.props.logged) {
+      this.props.history.push('/')
+      return null
+    } else {
+      return (
+        <React.Fragment>
+          <h4 className='center-align'>Login to edit</h4>
+          <div className='row'>
+            <form className='col s12' onSubmit={ this.submitHandler }>
+              <div className='row'>
+                <div className='col s3'></div>
+                <div className='input-field col s6'>
+                  <input
+                    id='email'
+                    type='email'
+                    className='validate'
+                    name='email'
+                    value={ this.state.email }
+                    onChange={ this.handleInput }
+                    required
+                  />
+                  <label htmlFor='email'>Email</label>
+                </div>
+                <div className='col s3'></div>
+              </div>
+              <div className='row'>
+                <div className='col s3'></div>
+                <div className='input-field col s6 '>
+                  <input
+                    id='password'
+                    type='password'
+                    className='validate'
+                    name='password'
+                    value={ this.state.password }
+                    onChange={ this.handleInput }
+                    required
+                  />
+                  <label htmlFor='password'>Password</label>
+                </div>
+                <div className='col s3'></div>
+              </div>
+              <div className='row'>
+                <div className='col s3' />
+                <div className='col s6'>
+                  <button className='btn waves-effect waves-light' type='submit' name='action'>
+                  submit<i className='material-icons right'>send</i></button>
+                </div>
+                <div className='col s3' />
+              </div>
+            </form>
+          </div>
+        </React.Fragment>
+      )
+    }
   }
 }
 
