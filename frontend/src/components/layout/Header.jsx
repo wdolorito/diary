356b538bde403c1535c8a920c658c84d75b0abd1
William Dolorito <wdolorito@gmail.com>
2021-08-21 20:06:34 -0700

    frontend/src/components/layout/Header.jsx:   add hamburger button for menu; need js initialization

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom'
 import logo from '../../logo.png'
 
 class Header extends Component {
+  componentDidUpdate() {
+
+  }
+
   render() {
     return(
       <nav>
         <div className='nav-wrapper'>
           <Link to='/' className='brand-logo' title='Home'><img alt='logo' src={ logo }/></Link>
-          <ul id="nav-mobile" className='right hide-on-med-and-down'>
+          <Link to='#' data-activates='nav-mobile' className='button-collapse'><i className='material-icons'>menu</i></Link>
+          <ul className='right hide-on-med-and-down'>
+            <li><Link to='latest' title='Latest'>Latest Posts</Link></li>
+            <li><Link to='about' title='About'>About</Link></li>
+            { (this.props.logged ) && <li><Link to='#' onClick={ this.props.doLogout }>Logout</Link></li>}
+          </ul>
+          <ul id='nav-mobile' className='side-nav'>
             <li><Link to='latest' title='Latest'>Latest Posts</Link></li>
             <li><Link to='about' title='About'>About</Link></li>
             { (this.props.logged ) && <li><Link to='#' onClick={ this.props.doLogout }>Logout</Link></li>}
