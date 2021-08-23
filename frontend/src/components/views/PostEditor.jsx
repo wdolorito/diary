18036397dfe74ddc33daddd2a6d5015275ec8c58
William Dolorito <wdolorito@gmail.com>
2021-08-22 17:34:23 -0700

    Prelim full form to submit new post
    frontend/package-lock.json:
    frontend/package.json:
      remove react-tinymce and replace with react-draft-wysiwyg
      add additional dependency, draftjs-to-html
    
    frontend/public/index.html:
      remove tinymce.min.js
    
    frontend/src/components/views/PostEditor.jsx:
      remove references to TinyMCE
      add react-draft-wysiwyg components
      add text input for Title and Summary
      add Editor component for Body
      prelim submitForm() onClick submit <button />

--- frontend/src/components/views/PostEditor.jsx
+++ frontend/src/components/views/PostEditor.jsx
@@ -1,41 +1,101 @@
-import { Component } from 'react'
-import TinyMCE from 'react-tinymce'
+import React from 'react'
+import { EditorState, convertToRaw } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg'
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import draftToHtml from 'draftjs-to-html'
 
-class PostEditor extends Component {
+class PostEditor extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
       title: '',
       summary: '',
-      body: ''
+      body: '',
+      editorState: null
     }
 
     this.baseState = this.state
+  }
+
+  componentDidMount() {
   }
 
   componentWillUnmount() {
     this.resetForm()
   }
 
-  resetForm = () => {
-    this.setState(this.baseState)
+  onInputStateChange = (e) => {
+    this.setState({ [ e.target.name ]: e.target.value })
+    console.log(e.target.value)
   }
 
-  handleEditorChange = (e) => {
-    console.log(e.target.getContent())
+  onEditorStateChange = (editorState) => {
+    const body = draftToHtml(convertToRaw(editorState.getCurrentContent()))
+    this.setState({ editorState, body })
+    console.log(body)
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    const title = this.state.title
+    const summary = this.state.summary
+    const body = this.state.body
+    const payload = { title, summary, body }
+    console.log(payload)
+  }
+
+  resetForm = () => {
+    this.setState(this.baseState)
   }
 
   render() {
     return (
       <div className='container'>
-        <TinyMCE
-          config={{
-            plugins: 'autolink link image lists preview',
-            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright'
-          }}
-          onChange={ this.handleEditorChange }
-        />
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              className='validate'
+              name='title'
+              value={ this.state.title }
+              onChange={ this.onInputStateChange }
+              placeholder='Title'
+              required
+            />
+            <label htmlFor='title'>Title</label>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              className='validate'
+              name='summary'
+              value={ this.state.summary }
+              onChange={ this.onInputStateChange }
+              placeholder='Summary'
+              required
+            />
+            <label htmlFor='summary'>Summary</label>
+          </div>
+        </div>
+
+        <div className='row'>
+          <Editor
+            editorState={ this.state.editorState }
+            onEditorStateChange={ this.onEditorStateChange }
+          />
+        </div>
+
+        <div className='row'>
+          <div className='col s3' />
+          <div className='col s6 center-align'>
+            <button className='btn waves-effect waves-light' onClick={ this.submitForm } name='action'>submit<i className='material-icons right'>send</i></button>
+          </div>
+          <div className='col s3' />
+        </div>
       </div>
     )
   }
