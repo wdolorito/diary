93a66b9b64d941c2aa311687564e374330315c2e
William Dolorito <wdolorito@gmail.com>
2022-07-07 11:09:40 -0700

    (nextjs) fe static context
    context/static_context.js:
      initial commit
      prelim fe fns to access be
    
    pages/_app.js:
      wrap static context

---
+++ nextjs/context/static_context.js
@@ -0,0 +1,154 @@
+import { createContext, useContext, useState } from 'react'
+
+import NetworkContext from './network_context'
+import AuthContext from './auth_context'
+
+const StaticContext = createContext()
+
+const StaticProvider = props => {
+  const { callAxios } = useContext(NetworkContext)
+  const { doRefresh, getAuthorization } = useContext(AuthContext)
+
+  const [ sections, setSections ] = useState([])
+  const [ page, setPage ] = useState({})
+  const [ pageReady, setPageReady ] = useState(false)
+ 
+  const staticLink = process.env.NEXT_PUBLIC_STATICLINK
+  const staticsLink = process.env.NEXT_PUBLIC_STATICSLINK
+
+  const getSections = () => {
+    const params = {
+      method: 'get',
+      url: staticsLink
+    }
+
+    const success = res => {
+      if(res.status === 200) {
+        setSections(res.data)
+      }
+    }
+
+    const fail = err => {
+      console.log('get statics error', staticsLink, err)
+      doRefresh()
+    }
+
+    callAxios(params, success, fail)
+  }
+
+  const createSection = payload => {
+    const headers = getAuthorization()
+
+    const params = {
+      method: 'post',
+      url: staticLink,
+      data: payload,
+      headers: headers
+    }
+
+    const success = res => {
+      const { status } = res
+      if(status === 201) {
+        getStatics()
+      }
+    }
+
+    const fail = err => {
+      console.log('post static error', err)
+      doRefresh()
+    }
+
+    callAxios(config, success, fail)
+  }
+
+  const getSection = section => {
+    const params = {
+      method: 'get',
+      url: staticLink + '/' + section,
+    }
+
+    const success = res => {
+      if(res.status === 200) {
+        setPage(res.data)
+        setPageReady(true)
+      }
+    }
+
+    const fail = err => {
+      console.log('get static error', err)
+      doRefresh()
+    }
+
+    callAxios(params, success, fail)
+  }
+
+  const updateSection = (payload, section) => {
+    const headers = getAuthorization()
+
+    const params = {
+      method: 'put',
+      url: staticLink + '/' + section,
+      data: payload,
+      headers: headers
+    }
+
+    const success = res => {
+      const { status } = res
+      if(status === 200) {
+        setPageReady(false)
+        getSection(section)
+      }
+    }
+
+    const fail = err => {
+      console.log('put static error', err)
+      doRefresh()
+    }
+
+    callAxios(config, success, fail)
+  }
+
+  const deleteSection = section => {
+    const headers = getAuthorization()
+
+    const params = {
+      method: 'delete',
+      url: staticLink + '/' + section,
+      headers: headers
+    }
+
+    const success = res => {
+      const { status } = res
+      if(status === 200) {
+        getStatics()
+      }
+    }
+
+    const fail = err => {
+      console.log('delete static error', err)
+      doRefresh()
+    }
+
+    callAxios(config, success, fail)
+  }
+
+  const value = {
+    sections,
+    page,
+    pageReady,
+    setPageReady,
+    getSections,
+    createSection,
+    getSection,
+    updateSection,
+    deleteSection
+  }
+
+  return (
+    <StaticContext.Provider value={ value }>
+      {props.children}
+    </StaticContext.Provider>
+  )
+}
+
+export { StaticContext as default, StaticProvider }
--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -3,15 +3,18 @@ import MainLayout from '../layout/main_layout'
 import { NetworkProvider } from '../context/network_context'
 import { AuthProvider } from '../context/auth_context'
 import { PostProvider } from '../context/post_context'
+import { StaticProvider } from '../context/static_context'
 
 function MyApp({ Component, pageProps }) {
   return (
     <NetworkProvider>
       <AuthProvider>
-      <PostProvider>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
+        <PostProvider>
+        <StaticProvider>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        </StaticProvider>
         </PostProvider>
       </AuthProvider>
     </NetworkProvider>
